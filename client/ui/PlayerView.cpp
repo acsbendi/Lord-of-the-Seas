214a407c5359656af4a28c0002781ac1b775cd9b
acsbendi <acsbendi28@gmail.com>
2018-12-09 15:29:47 +0100

    Implemented PlayerView.

---
+++ client/ui/PlayerView.cpp
@@ -0,0 +1,14 @@
+//
+// Created by acsbendi on 2018.12.09..
+//
+
+#include "PlayerView.hpp"
+
+void PlayerView::RegisterPlayer(PlayerProxy player) {
+    unsigned long size = associations.size();
+    associations.insert(player, colors[size]);
+}
+
+Color PlayerView::GetColorFor(PlayerProxy player) {
+    return associations.at(player);
+}
